f19caff4a0f26ac5268b4b359c38a100ad59d77d
Esaias Westberg <esaias@westbergs.se>
2023-09-26 11:45:07 +0200

    Added comments on integer requirement validation method

--- src/models/requirements/number/numberIntegerRequirement.ts
+++ src/models/requirements/number/numberIntegerRequirement.ts
@@ -2,6 +2,13 @@ import type { RequirementValidationError } from '../../../types/ValidationResult
 import Requirement from '../reqirement'
 
 export default class NumberIntegerRequirement extends Requirement {
+  /**
+   * Validate that the numeric value is an integer.
+   *
+   * @param value Numeric value to check.
+   * @returns A requirement validation error if the value is
+   * not an integer, otherwise an empty array.
+   */
   public validate(value: number): RequirementValidationError[] {
     // If the remainder after dividing by one is zero, it is an integer
     if (value % 1 === 0) return []
